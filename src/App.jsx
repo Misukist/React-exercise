1a73d3eea8820f31211cade207f0e4b1178ca426
Misukisti <misukisti@gmail.com>
2025-05-08 14:11:42 +0300

    Olion lisääminen

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,25 @@
-const Hello = (props) => {
-
-  console.log(props)
-  return (
-    <div>
-      <p>
-
-        Hello {props.name}, you are {props.age} years old
-      </p>
-    </div>
-  )
-}
-
 const App = () => {
-
-  const nimi = 'Pekka'
-  const ika = 10
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Greetings</h1>
-
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={nimi} age={ika} />
+      ...
     </div>
   )
 }
