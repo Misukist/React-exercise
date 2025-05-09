f5e39c612e81b4fd3c22080a66336afe317d78d6
Misukisti <misukisti@gmail.com>
2025-05-09 13:45:44 +0300

    Minus buttonin lisäys ja korjauksia koodiin

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,49 @@
 import {useState} from 'react'
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [ counter, setCounter ] = useState(0)
 
   const increaseByOne = () => setCounter(counter + 1)
+  console.log('increasing, value before', counter)
   
+  const decreaseByOne = () => setCounter(counter - 1)
+  console.log('increasing, value before', counter)
+
   const setToZero = () => setCounter(0)
+  console.log('resetting to zero, value before', counter)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increaseByOne}>
-        plus
-      </button>
-      <button onClick={setToZero}>
-        zero
-      </button>
+      <Display counter={counter}/>
+
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        onClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        onClick={decreaseByOne}
+        text='minus'
+      />           
     </div>
   )
+}
 
+const Display = (props) => {
+  return (
+    <div>{props.counter}</div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
 }
 
 export default App
