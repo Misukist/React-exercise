6bc65f258b708b9155db50acb6f0990689f36041
Misukisti <misukisti@gmail.com>
2025-05-10 15:39:50 +0300

    nappien funktionaalisuus muutettu

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,29 @@
 import {useState} from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
+const App = (props) => {
 
-  const increaseByOne = () => setCounter(counter + 1)
-  console.log('increasing, value before', counter)
-  
-  const decreaseByOne = () => setCounter(counter - 1)
-  console.log('increasing, value before', counter)
+  const [value, setValue] = useState(10) 
 
-  const setToZero = () => setCounter(0)
-  console.log('resetting to zero, value before', counter)
+  const setToValue = (newValue) => {
+    console.log('valuenow', newValue)
+    setValue(newValue)
+  }
+
+  const Button = (props) => (
+  <button onClick={props.onClick}>
+    {props.text}
+  </button>
+  )
 
   return (
     <div>
-      <Display counter={counter}/>
-
-      <Button
-        onClick={increaseByOne}
-        text='plus'
-      />
-      <Button
-        onClick={setToZero}
-        text='zero'
-      />     
-      <Button
-        onClick={decreaseByOne}
-        text='minus'
-      />           
+      {value}
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
 
-const Display = (props) => {
-  return (
-    <div>{props.counter}</div>
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
 
 export default App
